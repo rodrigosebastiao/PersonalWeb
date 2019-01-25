8cb1e7c2632d7023675245aba837f4ecd6901c8c
rodrigodss <rodrigodanielss@gmail.com>
2019-01-25 03:25:57 -0200

    alteração de diretório do index.html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,12 +2,13 @@ var startBtn = document.querySelector(".start");
 var startMenu = document.querySelector(".start-menu");
 var projectsModal = document.querySelector(".projects");
 
-var backgroundList = ["bg-sky.png", "bg-sky.png", "bg-wavetube.jpg"];
+var backgroundList = ["bg-sky.png", "bg-sky.png", "bg-wavetube.jpg", "bg-tree.jpg", "bg-wave.jpg"];
 
-startBtn.onclick = function(){
-    if(startMenu.style.display == "block"){
+
+startBtn.onclick = function () {
+    if (startMenu.style.display == "block") {
         startMenu.style.display = "none";
-    }else{
+    } else {
         startMenu.style.display = "block";
     }
 };
