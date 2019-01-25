52b43778d6f388379a524da93bcfc4f083d02c67
rodrigodss <rodrigodanielss@gmail.com>
2019-01-25 02:41:45 -0200

    header, menu, right click ok. Inicio JS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,13 @@
-var a = "#";
+var startBtn = document.querySelector(".start");
+var startMenu = document.querySelector(".start-menu");
+var projectsModal = document.querySelector(".projects");
+
+var backgroundList = ["bg-sky.png", "bg-sky.png", "bg-wavetube.jpg"];
+
+startBtn.onclick = function(){
+    if(startMenu.style.display == "block"){
+        startMenu.style.display = "none";
+    }else{
+        startMenu.style.display = "block";
+    }
+};
